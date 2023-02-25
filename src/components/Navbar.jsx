a9a8cbeb0fb2550205d7cd14614353b80aba6abe
Pradeepakumara K R <pradeepkumarkaggundi@gmail.com>
2023-02-24 14:42:31 -1200

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Stack } from "@mui/material";
+import { Avatar, Stack } from "@mui/material";
 // import { logo } from "../utils/constants";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import BrandLogo from '../assets/brand_logo2.png'
+import Profile from '../assets/profile.JPG'
 
 const Navbar = () => (
   <Stack
@@ -21,6 +22,10 @@ const Navbar = () => (
       <img src={BrandLogo} alt="logo" height={45} />
     </Link>
     <SearchBar />
+    <a href="https://personalpradeepprofile.netlify.app/">
+    <Avatar alt="Remy Sharp" src={Profile}   />
+    </a>
+    {/* sx={{ width: 56, height: 56 }} */}
   </Stack>
 );
 
