e48a81f9844f79ed4b6f8e83f9fec03e04499ecd
Pradeepakumara K R <pradeepkumarkaggundi@gmail.com>
2023-02-24 14:51:44 -1200

    Updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,7 +34,7 @@ const SearchBar = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}    
         />
-        <IconButton type='submit' sx={{p:'10px', color:'red'}}>
+        <IconButton type='submit' sx={{p:'6px', color:'red'}}>
             <Search />
         </IconButton>
     </Paper>
